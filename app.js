f3f029e642b569bd0fb779c865e1d291def1ffd8
watcharaphol.d <dangpiem@hotmail.com>
2024-06-23 13:10:34 +0700

    Create grade calculation logic

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 // write code here
+let score = prompt("Please enter the student score:");
+
+if (score === "") alert("Please enter the score");
+else if (!isNaN(+score)) {
+    score = +score;
+    if (score >= 80) alert("Congratulations, Your grade is 'A'");
+    else if (score >= 70 && score <= 79) alert("Well done, Your grade is 'B'");
+    else if (score >= 60 && score <= 69) alert("Keep going, Your grade is 'C'");
+    else if (score >= 50 && score <= 59) alert("Need more practice, Your grade is 'D'");
+    else if (score >= 0 && score <= 49) alert("Not pass, Your grade is 'F'");
+    else alert(`Is that your score, actually? \nPlease recheck your score`);
+} else alert("The received score is not number");
